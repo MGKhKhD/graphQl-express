8db6b99d22cc0d8772c3b1b283069f022183b6b4
mohammad ghadir khoshkholgh dashtaki <ghadir@ece.ubc.ca>
2018-02-13 12:22:03 -0800

    basic CRUD operations

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,17 @@
 const resolvers = {
     Query: {
-        hello: (parent, args, {models}) => "hi"
+        allUsers: (parent, args, {models}) => models.User.findAll(),
+        getUser: (parent, args, {models}) => models.User.findOne( {where: {
+            username: args.username
+        }})
     },
+
+    Mutation: {
+        createUser: (parent, args, {models}) => models.User.create(args),
+        updateUser: (parent, {username, newUsername}, {models}) => models.User.update({username: newUsername}, 
+            {where: {username}}),
+        deleteUser: (parent, args, {models}) => models.User.destroy({where: args})
+    }
   };
 
 module.exports = resolvers;
--- schema.js
+++ schema.js
@@ -1,7 +1,20 @@
-
 const typeDefs = `
+    type User {
+        id: String!
+        username: String!
+        createdAt: String!
+        updatedAt: String!
+    }
+
     type Query {
-        hello: String!
+        allUsers: [User!]!
+        getUser(username: String!): User
+    }
+
+    type Mutation {
+        createUser(username: String!): User
+        updateUser(username: String!, newUsername: String!): [Int!]!
+        deleteUser(username: String!): Int!
     }
   `;
 
